434c2a1801010f48da55da1e0566b45197b30ee9
Beto4224 <herbertbenevides@gmail.com>
2024-01-10 19:38:24 -0300

    Implementado alerta de confirmação.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
+let numeroJogosAlugados = 1;
+
 function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
 
     if(imagem.classList.contains('dashboard__item__img--rented')){
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
+        confimacao = confirm('Deseja devolver o jogo ' + gameClicado.querySelector('.dashboard__item__name').textContent + '?');
+        if(confimacao){
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+            numeroJogosAlugados--;
+        }
+        
     } else {
-        imagem.classList.add('dashboard__item__img--rented');
-        botao.classList.add('dashboard__item__button--return');
-        botao.textContent = 'Devolver';
+        confimacao = confirm('Deseja alugar o jogo ' + gameClicado.querySelector('.dashboard__item__name').textContent + '?');
+        if(confimacao){
+            imagem.classList.add('dashboard__item__img--rented');
+            botao.classList.add('dashboard__item__button--return');
+            botao.textContent = 'Devolver';
+            numeroJogosAlugados++
+        }
     }
+    console.log(numeroJogosAlugados);
 }
